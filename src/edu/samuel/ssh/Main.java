c6ae65c369b60edea42fff917d404c861ea3da49
spuemer845 <spuemer845@g.educaand.es>
2025-11-25 18:08:27 +0100

    Proyecto Java creado desde Eclipse y gestionado por terminal (SSH listo)

---
+++ src/edu/samuel/ssh/Main.java
@@ -0,0 +1,16 @@
+package edu.samuel.ssh;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        //Investiga el uso de random y genera por pantalla los números de la lotería primitiva.
+        Random random = new Random();
+
+        System.out.println("Números de la Lotería Primitiva:");
+        // Genera 5 números principales
+
+        // Número complementario
+
+    }
+}
