8abeba9a42c94abb870ba492a31b236924e9e63a
spuemer845 <spuemer845@g.educaand.es>
2025-11-25 19:31:07 +0100

    Estructura Eclipse + Main con Lotería (inicial)

--- src/edu/samuel/ssh/Main.java
+++ src/edu/samuel/ssh/Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         //Investiga el uso de random y genera por pantalla los números de la lotería primitiva.
-        Random random = new Random(436);
+        Random random = new Random();
 
         System.out.println("Números de la Lotería Primitiva:");
         // Genera 5 números principales
