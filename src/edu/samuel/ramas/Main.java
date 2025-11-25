8e214096a3d0c29b16e11327ad0bab1315967f8c
spuemer845 <spuemer845@g.educaand.es>
2025-11-25 19:54:13 +0100

    Mensaje en consola (rama feature/mensaje)

--- src/edu/samuel/ramas/Main.java
+++ src/edu/samuel/ramas/Main.java
@@ -16,6 +16,7 @@ public class Main {
         System.out.println("Complementario: " + complementario);
         int suma = bolas.stream().mapToInt(Integer::intValue).sum();
         System.out.println("Suma de principales: " + suma);
+        
         System.out.println("¡Suerte en el sorteo, DAM!");
     }
 }
